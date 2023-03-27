b4cdb4dd2d88ecdb976de5616d751181108efc9b
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-03-27 15:48:39 -0400

    Jawsdb

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
+
 sequelize.sync({force : false}).then(() => {
     app.listen(PORT, () => {
         console.log('Server listening on: http://localhost:' + PORT);
